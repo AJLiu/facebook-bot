557b5383a9a790c12dbe518c3d8c3953d79b7df6
Anthony Liu <tlrnalra@gmail.com>
2016-03-22 06:38:35 -0400

    readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# facebook-bot
-My bot for facebook messenger
+# Facebook Bot
+My bot for facebook messenger. Add [my bot](https://www.facebook.com/thonybot.giphy) and message him with ```@help``` to get started.
+
+# Running locally
+```
+$ git clone https://github.com/AJLiu/facebook-bot
+$ cd facebook-bot
+$ npm install
+```
+Create a copy of ```config_example.json``` into ```config.json``` and enter in the username and password of a bot facebook account.
+```
+$ npm start
+```
+
+# Making plugins
+There is a hello world sample plugin in plugins/_sample. To activate a plugin, ```require``` it in app.js and call the plugin's run function in the api.listen loop.
---
+++ plugins/_sample/sampleplugin.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var keyword = "@hey!"; //command to activate plugin from chat
+
+module.exports.keyword = keyword;
+module.exports.name = "helloworld"; //name of plugin
+module.exports.description =  `type ${keyword} to say hello world`; //explanation of plugin
+
+module.exports.run = (api, message) => {
+  if (message.body.slice(0, keyword.length) === keyword) {
+    if (message.body.length > keyword.length + 1) {
+      helloworld(api, message);
+    } else {
+      api.sendMessage(module.exports.description, message.threadID);
+    }
+  }
+};
+
+
+//========================================================
+// Helper functions
+//========================================================
+
+function helloworld(api, message) {
+  api.sendMessage("hello world", message.threadID);
+}
